8b16d46b876f1a5d46c4664de424fdd9c2ab5f72
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-05-23 14:06:33 -0500

    fixed .env variables in connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: DB_PASSWORD,
-    database: DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;
